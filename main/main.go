c0e9cbb0b67a02a0f7ab1484dd54db420fcd0512
Nick Soetaert <nicksoetaert@gmail.com>
2019-08-18 22:05:04 -0700

    messing with bit boards

--- main/main.go
+++ main/main.go
@@ -2,16 +2,11 @@
 package main
 
 import (
-	"Yerba/board"
+	//"Yerba/board"
 	"Yerba/utils"
-	"fmt"
+	//"fmt"
 )
 
 func main() {
-	fmt.Println("e2e4")
-	b := board.Board{}
-	fmt.Println(b)
-
-
-	fmt.Println(utils.GetBoardKey())
+	utils.Test()
 }
--- utils/debug.go
+++ utils/debug.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	//"strconv"
+	"fmt"
 	"strconv"
 )
 /*
@@ -14,21 +16,56 @@ func GetBoardKey() uint64 {
 		{"x","x","x"," "," "," "," "," "},
 		{" "," "," "," "," "," "," "," "},
 		{" "," "," "," "," "," "," "," "},
-		{" "," "," "," "," "," "," "," "},
+		{" "," "," "," ","x"," "," "," "},
 		{" "," "," "," "," "," "," "," "},
 		{" "," "," "," "," "," "," "," "},
 	}
 	board[0][0]="y"
 
 	var result uint64 
-	var str string
+	//var str string
 
-	for i := 0; i < 64; i++ {
+	for i := uint8(0); i < 64; i++ {
 		if board[i/8][i%8] != " " {
-			result += 0
+			result += 1 << i
 		}
 	}
 
 
 	return result
+}
+
+
+func GetSingleBB(key uint64){
+	board := [8][8]string{{}}
+	str := strconv.FormatUint(key, 2)
+
+
+	//fmt.Println(len(str))
+	//fmt.Println(str[63])
+
+	//for i:= len(str)-1; i >= 0; i-- {
+	//bug: str isn't len(64) unless there's pieces on a1 and h8
+
+	for i:= 0; i < len(str); i++{
+
+		if str[i] == '1'{
+			board[i/8][i%8] = "x"
+		} else {
+			board[i/8][i%8] = " "
+		}
+	}
+	
+	for _, row := range board{
+		for _, cell := range row{
+			print("|",cell)
+		}
+		fmt.Println("|")
+	}
+}
+
+func Test() {
+	x := GetBoardKey()
+	GetSingleBB(x)
+	fmt.Println("a")
 }
