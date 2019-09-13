36e0075058f03925e789c03d8597f6e834350823
Nick Soetaert <nicksoetaert@gmail.com>
2019-09-12 23:59:29 -0700

    knight attack squares

--- board/movegen.go
+++ board/movegen.go
@@ -1,9 +1,19 @@
 package board
 
-const knightmoveshape BinaryBoard = 43234889994
+import (
+	"math/bits"
+)
 
-func KnightMoves(knights *BinaryBoard) BinaryBoard {
+//KnightAttacks returns a BB of all squares that knights of a given color can attack, regardless of other pieces on the board.
+func KnightAttacks(knights BinaryBoard) BinaryBoard {
 
-	return *knights
-
+	var attackers BinaryBoard
+	count := bits.OnesCount64(uint64(knights))
+	//count must be used, else loop range will decrement with each pass.
+	for i := 0; i < count; i++ {
+		singleKnightPosition := uint8(bits.TrailingZeros64(uint64(knights)))
+		attackers |= knightMoves[singleKnightPosition] //get all squares that knight can attack
+		knights ^= (1 << singleKnightPosition)         //now clear that knight for the next loop iteration
+	}
+	return attackers
 }
--- main/main.go
+++ main/main.go
@@ -2,7 +2,7 @@
 package main
 
 import (
-	//"Yerba/board"
+	"Yerba/board"
 	"Yerba/utils"
 	"fmt"
 )
@@ -10,27 +10,13 @@ import (
 func main() {
 	fmt.Println("")
 	//fmt.Printf("%64b\n", board.OneFile)
-	//utils.PrintBinaryBoard(board.KingSide)
-	//myBoard := board.SetUpBoard()
 
-	//fmt.Printf("%c\n", board.BlackPawn)
+	//fmt.Printf("0x%016x\n", x)
 
-	//board.PrintBoard(myBoard)
+	x := board.SetUpBoard()
 
-	x := utils.GetBoardKey()
+	k := x.Knights & x.Black
 
-	utils.PrintBinaryBoard(x)
-
-	fmt.Printf("0x%016x\n", x)
-
-	//x := board.BinaryBoard(utils.GetBoardKey())
-
-	//utils.PrintBinaryBoard(board.BishopMult[63])
-
-	//fmt.Println(x == (board.C2 | board.B3))
-
-	//kboard := board.Board{0, x, 0, 0, 0, 0, x, 0, true}
-
-	//board.PrintBoard(kboard)
+	utils.PrintBinaryBoard(board.KnightAttacks(k))
 
 }
