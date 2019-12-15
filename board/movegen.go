a3f3cd8002d2a921dacedb764776e43c85533d59
Nick Soetaert <nicksoetaert@gmail.com>
2019-12-14 23:40:48 -0800

    comments

--- board/movegen.go
+++ board/movegen.go
@@ -8,12 +8,13 @@ import (
 func KnightAttacks(knights BinaryBoard) BinaryBoard {
 
 	var attackers BinaryBoard
+	//Get number of squares that the knight can attack
 	count := bits.OnesCount64(uint64(knights))
 	//count must be used, else loop range will decrement with each pass.
 	for i := 0; i < count; i++ {
 		singleKnightPosition := uint8(bits.TrailingZeros64(uint64(knights)))
-		attackers |= knightMoves[singleKnightPosition] //get all squares that knight can attack
-		knights ^= (1 << singleKnightPosition)         //now clear that knight for the next loop iteration
+		attackers |= knightMoves[singleKnightPosition] //get current square that knight can attack
+		knights ^= 1 << singleKnightPosition         //now clear that knight for the next loop iteration
 	}
 	return attackers
 }
