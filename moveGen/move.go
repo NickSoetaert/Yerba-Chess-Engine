7ceaa0598169ad4150e0bec18a452b91feef200a
Nick Soetaert <nicksoetaert@gmail.com>
2020-03-08 19:36:41 -0700

    type Move spec

--- moveGen/move.go
+++ moveGen/move.go
@@ -1,8 +1,13 @@
 package moveGen
 
+//Type Move specifications:
 //Origin square bits:	   	0-5
 //Destination square bits: 	6-11
 //Special move bits:		12-14
+//Who's turn:				15
+
+//Origin/dest square bits:
+//0 represents A1, and 64 represents H8
 
 //Special move flag bits:
 //Denotes if a pawn promotion, castling, or en passant is taking place.
@@ -15,8 +20,14 @@ package moveGen
 //110 - rook promotion
 //111 - queen promotion
 
-//origin	dest		special move	unused
-//111111	111111		111				1
+//Turn bits:
+//0 - white to move
+//1 - black to move
+
+//000000 000001 000 1 : A white piece started on A1 and moved to A2.
+//000000 000000 010 1 : white castled kingside
+//010011 010100 001 0 : black captured en passant from d4 to e3
+
 type Move uint16
 
 //Given a starting board and a move, return the resulting board.
